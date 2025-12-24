6cb28c7db8d3920bfd36c73971025ca80b6c3afd
nadinehc <nadinehagechehade@gmail.com>
2025-12-24 12:27:42 +0100

    create main.py

---
+++ main.py
@@ -0,0 +1,7 @@
+from datasets import datasetA, datasetB, datasetC, datasetD, visualize_dataset
+from sweeping import visualize_hull
+
+if __name__ == "__main__":
+    # visualize_dataset()
+
+    visualize_hull(100, "B")
